31b474ad97c841787a14fb86c891684847420b29
mohamed helmy <mohamedhelmy1531@gmail.com>
2023-09-02 21:00:00 +0300

    fixing port

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(compression());
 app.use(
   cors({
     origin: [
-      "http://localhost:3001",
+      "http://localhost:3000",
       "https://helmy-social-media.onrender.com",
     ],
   })
